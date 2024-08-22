3f17278f69336fb353cd6ec51c7073f51a88ae37
Gad-Ongoro <gadongoro1@gmail.com>
2024-08-22 13:50:20 +0300

    registered new components to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import DashBoard from './components/dashboard/DashBoard';
+import SignUp from './components/Auth/SignUp';
+import SignIn from './components/Auth/SignIn';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+				<Route path="/dashboard" element={<DashBoard />} />
+				<Route path="/signup" element={<SignUp />} />
+				<Route path="/signin" element={<SignIn />} />
+      </Routes>
     </div>
   );
 }
